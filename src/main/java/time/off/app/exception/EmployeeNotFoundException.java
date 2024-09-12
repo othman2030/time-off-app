cf1d4c73e1d11b413c996d81e6393eb3d4799131
unknown <othman.2030@gmail.com>
2024-09-12 12:13:11 +0200

    change controllers

--- src/main/java/time/off/app/exception/EmployeeNotFoundException.java
+++ src/main/java/time/off/app/exception/EmployeeNotFoundException.java
@@ -1,10 +1,7 @@
 package time.off.app.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.NOT_FOUND)
-public class DataNotFoundException extends RuntimeException{
-    public DataNotFoundException(String s) {
+public class EmployeeNotFoundException extends RuntimeException{
+    public EmployeeNotFoundException(String message) {
+        super(message);
     }
 }
